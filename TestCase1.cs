d516779435991206056078a9a365828a6d2c9220
mariyaDobreva <mariya.dobreva@gmail.com>
2020-01-17 19:52:57 +0200

    Add files via upload

---
+++ TestCase1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscribe
+{
+	class CSS						
+    {
+        IWebDriver m_driver;
+
+        [Test]
+		public void SubscribeDemo()
+        {
+            m_driver = new ChromeDriver("C:\Users\User\Desktop");
+            m_driver.Url = "https://www.theinformation.com";
+            m_driver.Manage().Window.Maximize();
+
+             
+			// Store locator values of email text box and sign up button				
+            IWebElement emailTextBox = m_driver.FindElement(By.CssSelector("input[id=footer-email]"));
+            IWebElement signUpButton = m_driver.FindElement(By.CssSelector("input[css=.col-md-4 > .submit]"));
+
+            emailTextBox.SendKeys("pesho@mail.com");
+            signUpButton.Click();
+
+             
+        }
+    }
+}			
